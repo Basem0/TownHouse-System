fa2a35e27f7fa8df4ed65e8d41b671405e8c4698
Mohamed Zkrallah <54005330+muhammadzkralla@users.noreply.github.com>
2023-12-19 19:36:53 +0200

    Delete Zfire directory

--- Zfire/app/src/main/java/com/zkrallah/z_fire/domain/models/Gas.kt
+++
@@ -1,5 +0,0 @@
-package com.zkrallah.z_fire.domain.models
-
-data class Gas(
-    val value: String? = null
-)
--- Zfire/app/src/main/java/com/zkrallah/z_fire/domain/usecase/GetTemp.kt
+++
@@ -1,7 +0,0 @@
-package com.zkrallah.z_fire.domain.usecase
-
-import com.zkrallah.z_fire.domain.repositories.MainRepository
-
-class GetTemp(private val mainRepository: MainRepository) {
-    suspend operator fun invoke() = mainRepository.fetchTemp()
-}
